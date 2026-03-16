ce6e85d88d31885f49ddd28d26537653e79fd35e
zm0008 <0412005@schoolsnet.act.edu.au>
2026-03-17 00:36:52 +1100

    Nested assessment under unit in course viewer

--- UnitOutlineDjango/MyApp/models.py
+++ UnitOutlineDjango/MyApp/models.py
@@ -5,6 +5,9 @@ class teacher(models.Model):
     Name = models.CharField(max_length=25)
     Area = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.Name
+
 class scalinggroup(models.Model):
     Name = models.IntegerField()
 
@@ -12,15 +15,24 @@ class course(models.Model):
     Name = models.CharField(max_length=20)
     ScalingGroup = models.ForeignKey(scalinggroup, null=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return self.Name
+
 class unit(models.Model):
     Name = models.CharField(max_length=30)
     Course = models.ForeignKey(course, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Name
 
 class assessment(models.Model):
     Unit = models.ForeignKey(unit, on_delete=models.CASCADE)
     Name = models.CharField(max_length=50)
     StartDate = models.DateField()
     DueDate = models.DateField()
+
+    def __str__(self):
+        return self.Name
 
 class student(models.Model):
     Tertiary = "T"
